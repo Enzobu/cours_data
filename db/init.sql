877094286c96d797ee44bbd1ca7ae94161771ad4
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-31 00:31:45 +0100

    add tables for new data

--- db/init.sql
+++ db/init.sql
@@ -1,20 +1,97 @@
-CREATE TABLE IF NOT EXISTS data (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    city VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    price VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS cleanData (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    city VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    price FLOAT,
-    unitPrice VARCHAR(255)
-);
-
-CREATE TABLE links (
+CREATE TABLE IF NOT EXISTS links (
     id INT AUTO_INCREMENT PRIMARY KEY,
     country VARCHAR(255) NOT NULL,
     country_link VARCHAR(255) NOT NULL
 );
+
+
+
+
+-- cost_of_life
+CREATE TABLE IF NOT EXISTS cost_of_life_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(255),
+    description TEXT,
+    value VARCHAR(255),
+    unit VARCHAR(255)
+);
+
+-- CREATE TABLE IF NOT EXISTS cost_of_life_cleanData (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     country VARCHAR(255) NOT NULL,
+--     description TEXT NOT NULL,
+--     price FLOAT,
+--     unitPrice VARCHAR(255)
+-- );
+
+
+-- crime
+CREATE TABLE IF NOT EXISTS crime_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(255),
+    description TEXT,
+    value VARCHAR(255),
+    unit VARCHAR(255)
+);
+
+-- CREATE TABLE IF NOT EXISTS crime_cleanData (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     country VARCHAR(255) NOT NULL,
+--     description TEXT NOT NULL,
+--     price FLOAT,
+--     unitPrice VARCHAR(255)
+-- );
+
+
+-- health_care
+CREATE TABLE IF NOT EXISTS health_care_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(255),
+    description TEXT,
+    value VARCHAR(255),
+    unit VARCHAR(255)
+);
+
+-- CREATE TABLE IF NOT EXISTS health_care_cleanData (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     country VARCHAR(255) NOT NULL,
+--     description TEXT NOT NULL,
+--     price FLOAT,
+--     unitPrice VARCHAR(255)
+-- );
+
+
+-- pollution
+CREATE TABLE IF NOT EXISTS pollution_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(255),
+    description TEXT,
+    value VARCHAR(255),
+    unit VARCHAR(255)
+);
+
+-- CREATE TABLE IF NOT EXISTS pollution_cleanData (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     country VARCHAR(255) NOT NULL,
+--     description TEXT NOT NULL,
+--     price FLOAT,
+--     unitPrice VARCHAR(255)
+-- );
+
+
+-- quality_of_life
+CREATE TABLE IF NOT EXISTS quality_of_life_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(255),
+    description TEXT,
+    value VARCHAR(255),
+    unit VARCHAR(255)
+);
+
+-- CREATE TABLE IF NOT EXISTS quality_of_life_cleanData (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     country VARCHAR(255) NOT NULL,
+--     description TEXT NOT NULL,
+--     price FLOAT,
+--     unitPrice VARCHAR(255)
+-- );
