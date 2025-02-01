12c0371fda70bd87dd2d18b11d44cdec4ca4909f
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-01 20:03:20 +0100

    Create getPollutionCleanData.py

---
+++ app/src/getCleanData/getPollutionCleanData.py
@@ -0,0 +1,19 @@
+import pandas as pd # type: ignore
+import re
+
+from db.getDataFromDb import getDataFromDb
+from db.flushCleanData import flushPollutionData
+
+def getPollutionCleanData():
+    data = getDataFromDb('data_pollution')
+
+    df = pd.DataFrame(data, columns=["id", "country", "description", "value", "unit"])
+
+    df = df.dropna()
+    df = df.drop_duplicates()
+    # Supprimer les ligne pour lesquelles il y a '?' dans une des colonnes
+    df = df[~df.applymap(lambda x: '?' in str(x)).any(axis=1)]
+    # Supprimer les ligne pour lesquelles la colonne 'value' n'est pas un nombre
+    df = df[pd.to_numeric(df['value'], errors='coerce').notna()]
+    
+    flushPollutionData(df)
