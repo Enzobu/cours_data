86c2a47d0eec46939800e2fc8f3ca5c442f32ce6
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-01 20:02:53 +0100

    Update cleanData.py

--- app/src/cleanData.py
+++ app/src/cleanData.py
@@ -1,47 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import re
-import mysql.connector
+from getCleanData.getCostOfLifeCleanData import getCostOfLifeCleanData
+from getCleanData.getCrimeCleanData import getCrimeCleanData
+from getCleanData.getHealthCareCleanData import getHealthCareCleanData
+from getCleanData.getPollutionCleanData import getPollutionCleanData
+from getCleanData.getQualityOfLifeCleanData import getQualityOfLifeCleanData
 
-if __name__ == '__main__':
+print(" ========== Début de la récupération des datas relative à \"getCostOfLifeCleanData\" ========== ")
+getCostOfLifeCleanData()
 
+print("\n\n ========== Début de la récupération des datas relative à \"getCrimeCleanData\" ========== ")
+getCrimeCleanData()
 
-    cursor.execute("SELECT * FROM data")
-    data = cursor.fetchall()
+print("\n\n ========== Début de la récupération des datas relative à \"getHealthCareCleanData\" ========== ")
+getHealthCareCleanData()
 
-    df = pd.DataFrame(data, columns=["id", "city", "description", "price"])
+print("\n\n ========== Début de la récupération des datas relative à \"getPollutionCleanData\" ========== ")
+getPollutionCleanData()
 
-    df = df.dropna()
-    df = df.drop_duplicates()
+print("\n\n ========== Début de la récupération des datas relative à \"getQualityOfLifeCleanData\" ========== ")
+getQualityOfLifeCleanData()
 
-    # df_bis = df[~df["price"].str.contains(r"(\d+\.\d+)\s+[^\s]+", na=False)]
-
-    df = df[df["price"].str.contains(r"(\d+\.\d+)\s+[^\s]+", na=False)]
-
-    def extract_price_and_currency(price):
-        match = re.search(r"(\d+\.\d+)\s+([^\s]+)", price)
-        if match:
-            numeric_price = float(match.group(1))
-            currency = match.group(2)
-            return numeric_price, currency
-        return -1, -1
-
-    df[["price", "unitPrice"]] = df["price"].apply(
-        lambda p: pd.Series(extract_price_and_currency(p))
-    )
-
-    for index, row in df.iterrows():
-        city = row['city']
-        description = row['description']
-        price = row['price']
-        unitPrice = row['unitPrice']
-        sql = "INSERT INTO cleanData (city, description, price, unitPrice) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (city, description, price, unitPrice))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    print("Les données ont été nettoyées et insérées dans la table cleanData.")
-    # print(f"longueur de df bis {len(df_bis)}")
+print("Processus terminé avec succès.")
