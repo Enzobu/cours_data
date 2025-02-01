9eef3f51a546c504440e6a61c2cc508c14d85416
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-01 12:10:32 +0100

    update all gatData files

--- app/src/getData/getHealthCareData.py
+++ app/src/getData/getHealthCareData.py
@@ -5,34 +5,53 @@ import re
 import time
 
 
-def getHealthCareData(urls, base_url):
+def getHealthCareData(urls, base_url, sleep):
     col0 = []
     col1 = []
     col2 = []
     col3 = []
+
+    url_count = len(urls)
 
     n = 1
 
     for url in urls:
         country = re.search(r"country=([^&]+)", url).group(1)
 
-        print(f"Récupération pour {country}. No = {n}")
+        part = re.search(r"(?<=https://www\.numbeo\.com/)([^/]+)", base_url).group(1)
+        print(f"{round((n/url_count*100), 2):.2f}% --- N° = {n:03d}/{url_count} ---- {part} ---- Récupération pour {country}.")
+        
+        try:
+            response = requests.get(base_url + url)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        response = requests.get(base_url + url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+            table = soup.select('table.table_builder_with_value_explanation')[0]
+            trList = table.select('tr')
 
-        table = soup.select('table.table_builder_with_value_explanation')[0]
-        trList = table.select('tr')
+            for tr in trList:
+                try:
+                    if tr.select('td'):
+                        td_description = tr.select('td.columnWithName')[0].text
+                        td_value = re.sub(r'\n.*', '', tr.select('td.indexValueTd')[0].text)
+                        col0.append(td_description)
+                        col1.append(td_value)
+                        col2.append("%")
+                        col3.append(country)
+                except:
+                    col0.append("Erreur")
+                    col1.append("Erreur")
+                    col2.append("Erreur")
+                    col3.append(country)
+                    print(f"Erreur lors d'une boucle pour {country}...")
 
-        for tr in trList:
-            if tr.select('td'):
-                td_description = tr.select('td.columnWithName')[0].text
-                td_value = re.sub(r'\n.*', '', tr.select('td.indexValueTd')[0].text)
-                col0.append(td_description)
-                col1.append(td_value)
-                col2.append("%")
-                col3.append(country)
-        time.sleep(0.5)
+        except:
+            col0.append("Erreur")
+            col1.append("Erreur")
+            col2.append("Erreur")
+            col3.append(country)
+            print(f"Erreur lors de la récupération pour {country}...")
+
+        time.sleep(sleep)
         n+=1
 
     result = {
--- app/src/getData/getQualityOfLifeData.py
+++ app/src/getData/getQualityOfLifeData.py
@@ -5,34 +5,53 @@ import re
 import time
 
 
-def getQualityOfLifeData(urls, base_url):
+def getQualityOfLifeData(urls, base_url, sleep):
     col0 = []
     col1 = []
     col2 = []
     col3 = []
+
+    url_count = len(urls)
 
     n = 1
 
     for url in urls:
         country = re.search(r"country=([^&]+)", url).group(1)
 
-        print(f"Récupération pour {country}. No = {n}")
+        part = re.search(r"(?<=https://www\.numbeo\.com/)([^/]+)", base_url).group(1)
+        print(f"{round((n/url_count*100), 2):.2f}% --- N° = {n:03d}/{url_count} ---- {part} ---- Récupération pour {country}.")
 
-        response = requests.get(base_url + url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        try:
+            response = requests.get(base_url + url)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        table = soup.select('table')[2]
-        trList = table.select('tr')
-        for tr in trList:
-            # print(True if tr.select('a.discreet_link') and not tr.select('a.hide_smaller_than_600') else False)
-            if tr.select('a.discreet_link') and not tr.select('a.hide_smaller_than_600'):
-                td_description = tr.select('a.discreet_link')[0].text
-                td_value = tr.select('td')[1].text
-                col0.append(td_description)
-                col1.append(td_value)
-                col2.append("%")
-                col3.append(country)
-        time.sleep(0.5)
+            table = soup.select('table')[2]
+            trList = table.select('tr')
+            
+            for tr in trList:
+                try:
+                    if tr.select('a.discreet_link') and not tr.select('a.hide_smaller_than_600'):
+                        td_description = tr.select('a.discreet_link')[0].text
+                        td_value = tr.select('td')[1].text
+                        col0.append(td_description)
+                        col1.append(td_value)
+                        col2.append("%")
+                        col3.append(country)
+                except:
+                    col0.append("Erreur")
+                    col1.append("Erreur")
+                    col2.append("Erreur")
+                    col3.append(country)
+                    print(f"Erreur lors d'une boucle pour {country}...")
+
+        except:
+            col0.append("Erreur")
+            col1.append("Erreur")
+            col2.append("Erreur")
+            col3.append(country)
+            print(f"Erreur lors de la récupération pour {country}...")
+
+        time.sleep(sleep)
         n+=1
 
     result = {
