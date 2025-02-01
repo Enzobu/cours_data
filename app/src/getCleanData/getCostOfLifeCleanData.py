d51c1f714bade3ae3ce055c17f01869077b029a4
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-01 20:03:13 +0100

    Create getCostOfLifeCleanData.py

---
+++ app/src/getCleanData/getCostOfLifeCleanData.py
@@ -0,0 +1,29 @@
+import pandas as pd # type: ignore
+import re
+
+from db.getDataFromDb import getDataFromDb
+from db.flushCleanData import flushCostOfLifeData
+
+def getCostOfLifeCleanData():
+    data = getDataFromDb('data_cost_of_life')
+
+    df = pd.DataFrame(data, columns=["id", "country", "description", "value", "unit"])
+
+    df = df.dropna()
+    df = df.drop_duplicates()
+
+    df = df[df["value"].str.contains(r"(\d+\.\d+)\s+[^\s]+", na=False)]
+
+    def extract_value_and_currency(value):
+        match = re.search(r"(\d+\.\d+)\s+([^\s]+)", value)
+        if match:
+            numeric_value = float(match.group(1))
+            currency = match.group(2)
+            return numeric_value, currency
+        return -1, -1
+
+    df[["value", "unitValue"]] = df["value"].apply(
+        lambda p: pd.Series(extract_value_and_currency(p))
+    )
+
+    flushCostOfLifeData(df)
