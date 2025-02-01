66e9c1cd235d69a45d848dded58861833ed410bc
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-01 20:03:23 +0100

    Create getQualityOfLifeCleanData.py

---
+++ app/src/getCleanData/getQualityOfLifeCleanData.py
@@ -0,0 +1,19 @@
+import pandas as pd # type: ignore
+import re
+
+from db.getDataFromDb import getDataFromDb
+from db.flushCleanData import flushQualityOfLifeData
+
+def getQualityOfLifeCleanData():
+    data = getDataFromDb('data_quality_of_life')
+
+    df = pd.DataFrame(data, columns=["id", "country", "description", "value", "unit"])
+
+    df = df.dropna()
+    df = df.drop_duplicates()
+    # Supprimer les ligne pour lesquelles il y a '?' dans une des colonnes
+    df = df[~df.applymap(lambda x: '?' in str(x)).any(axis=1)]
+    # Supprimer les ligne pour lesquelles la colonne 'value' n'est pas un nombre
+    df = df[pd.to_numeric(df['value'], errors='coerce').notna()]
+
+    flushQualityOfLifeData(df)
