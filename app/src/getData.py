c79002b685fe2596beb9fe2842c87e8437c39ad4
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-01 12:10:15 +0100

    Update getData.py

--- app/src/getData.py
+++ app/src/getData.py
@@ -1,21 +1,13 @@
-import mysql.connector # type: ignore
 import pandas as pd # type: ignore
 
 from getData.getCostOfLifeData import getCostOfLifeData
-
 from getData.getCrimeData import getCrimeData
 from getData.getHealthCareData import getHealthCareData
 from getData.getPollutionData import getPollutionData
 from getData.getQualityOfLifeData import getQualityOfLifeData
 
-conn = mysql.connector.connect(
-    host='mysql',
-    user='root',
-    password='root',
-    database='cours_data',
-    port=3306
-)
-cursor = conn.cursor()
+from getLinksFromDb import getLinksFromDb
+from flush import flush
 
 base_urls = {
     "costOfLife": "https://www.numbeo.com/cost-of-living/",
@@ -25,68 +17,43 @@ base_urls = {
     "qualityOfLife": "https://www.numbeo.com/quality-of-life/",
 }
 
-urls = []
+urls = getLinksFromDb()
 
-cursor.execute("SELECT country_link FROM links")
-data = cursor.fetchall()
-for row in data:
-    urls.append(row[0])
+sleep = 2
 
-dfs = []
-
-print("=============================================")
-df_costOfLife = getCostOfLifeData(urls, base_urls['costOfLife'])
-dfs.append({
+print(" ========== Début de la récupération des datas relative à \"costOfLife\" ========== ")
+df_costOfLife = getCostOfLifeData(urls, base_urls['costOfLife'], sleep)
+flush({
     "table": "cost_of_life_data",
     "df": df_costOfLife,
 })
 
-print("=============================================")
-df_crime = getCrimeData(urls, base_urls['crime'])
-dfs.append({
+print(" ========== Début de la récupération des datas relative à \"crime\" ========== ")
+df_crime = getCrimeData(urls, base_urls['crime'], sleep)
+flush({
     "table": "crime_data",
     "df": df_crime,
 })
 
-print("=============================================")
-df_healthCare = getHealthCareData(urls, base_urls['healthCare'])
-dfs.append({
+print(" ========== Début de la récupération des datas relative à \"healthCare\" ========== ")
+df_healthCare = getHealthCareData(urls, base_urls['healthCare'], sleep)
+flush({
     "table": "health_care_data",
     "df": df_healthCare,
 })
 
-print("=============================================")
-df_pollution = getPollutionData(urls, base_urls['pollution'])
-dfs.append({
+print(" ========== Début de la récupération des datas relative à \"pollution\" ========== ")
+df_pollution = getPollutionData(urls, base_urls['pollution'], sleep)
+flush({
     "table": "pollution_data",
     "df": df_pollution,
 })
 
-print("=============================================")
-df_qualityOfLife = getQualityOfLifeData(urls, base_urls['qualityOfLife'])
-dfs.append({
+print(" ========== Début de la récupération des datas relative à \"qualityOfLife\" ========== ")
+df_qualityOfLife = getQualityOfLifeData(urls, base_urls['qualityOfLife'], sleep)
+flush({
     "table": "quality_of_life_data",
     "df": df_qualityOfLife,
 })
-
-
-for df in dfs:
-    for index, row in df["df"].iterrows():
-        table = df["table"]
-
-        country = row['country']
-        description = row['description']
-        value = row['value']
-        unit = row['unit']
-        
-
-        sql = f"INSERT INTO {table} (country, description, value, unit) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (country, description,value, unit))
-
-    print(f"Les données ont été insérées avec succès dans la table {table}")
-    conn.commit()
-
-cursor.close()
-conn.close()
 
 print("Processus terminé avec succès.")
