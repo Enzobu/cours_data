db58b09e4206d1dd92862ab691868ce0e7cb027d
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-03-18 11:45:26 +0100

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -93,3 +93,76 @@ CREATE TABLE IF NOT EXISTS clean_data_quality_of_life (
     value FLOAT NOT NULL,
     unit VARCHAR(255) NOT NULL
 );
+
+
+-- data_crime_index
+CREATE TABLE IF NOT EXISTS data_crime_index (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(255),
+    crime_index TEXT,
+);
+
+CREATE TABLE IF NOT EXISTS clean_data_crime_index (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(255) NOT NULL,
+    crime_index TEXT NOT NULL,
+);
+
+
+-- matrix_cost_of_life
+CREATE TABLE matrix_cost_of_life (
+    country VARCHAR(255),
+    meal_inexpensive FLOAT,
+    meal_for_two FLOAT,
+    mcmeal FLOAT,
+    domestic_beer FLOAT,
+    imported_beer FLOAT,
+    cappuccino FLOAT,
+    coke_pepsi FLOAT,
+    water_small FLOAT,
+    milk FLOAT,
+    bread FLOAT,
+    rice FLOAT,
+    eggs FLOAT,
+    cheese FLOAT,
+    chicken FLOAT,
+    beef FLOAT,
+    apples FLOAT,
+    banana FLOAT,
+    oranges FLOAT,
+    tomato FLOAT,
+    potato FLOAT,
+    onion FLOAT,
+    lettuce FLOAT,
+    water_large FLOAT,
+    non_alcoholic_wine FLOAT,
+    cigarettes FLOAT,
+    local_transport_ticket FLOAT,
+    monthly_pass FLOAT,
+    taxi_start FLOAT,
+    taxi_per_km FLOAT,
+    taxi_waiting FLOAT,
+    gasoline FLOAT,
+    volkswagen_golf FLOAT,
+    toyota_corolla FLOAT,
+    utilities FLOAT,
+    mobile_plan FLOAT,
+    internet FLOAT,
+    fitness_club FLOAT,
+    tennis_court FLOAT,
+    cinema FLOAT,
+    preschool FLOAT,
+    primary_school FLOAT,
+    jeans FLOAT,
+    summer_dress FLOAT,
+    nike_shoes FLOAT,
+    leather_shoes FLOAT,
+    apt_1bed_city FLOAT,
+    apt_1bed_out FLOAT,
+    apt_3bed_city FLOAT,
+    apt_3bed_out FLOAT,
+    price_sq_m_city FLOAT,
+    price_sq_m_out FLOAT,
+    net_salary FLOAT,
+    crime_index FLOAT
+);
